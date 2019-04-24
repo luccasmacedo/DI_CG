1ffc1dd06ac17e1a0a45301ae1cc19da57c0c7a1
luccasmacedo <lukkas392@gmail.com>
2019-04-24 12:07:04 -0300

    I think is it

--- D1_20191_Base/main.cpp
+++ D1_20191_Base/main.cpp
@@ -9,6 +9,7 @@
 #include "extras.h"
 #include <../shared/glcFPSViewer.h>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -16,19 +17,19 @@ using namespace std;
 //  Você poderá utilizá-las adicionando novos métodos (de acesso por exemplo) ou usar suas próprias estruturas.
 class vertice
 {
-    public:
-        float x,y,z;
-        float espessura;
+public:
+    float x,y,z;
+    float espessura;
 };
 
 class triangle
 {
-    public:
-        vertice v[3];
+public:
+    vertice v[3];
 };
 
 /// Globals
-float zdist = 10.0;
+float zdist = 30.0;
 
 //Coordenada z do ponto criado
 float alturaZPonto = 1.0;
@@ -39,7 +40,7 @@ int click = 0;
 float espessuraGlobal = 1;
 bool fullSreen = false;
 vertice oldVector[4];
-
+bool entraIf = false;
 int indexGrupoAtual = 0;
 
 vector<list<vertice>> grupos;
@@ -62,7 +63,8 @@ void init(void)
   +----> v_1
   v_0
 */
-void CalculaNormal(triangle t, vertice *vn){
+void CalculaNormal(triangle t, vertice *vn)
+{
     vertice v_0 = t.v[0],
             v_1 = t.v[1],
             v_2 = t.v[2];
@@ -91,7 +93,8 @@ void CalculaNormal(triangle t, vertice *vn){
     vn->z /= len;
 }
 
-void CalculoOrtogonal(vertice v1, vertice v2, vertice* vn){
+void CalculoOrtogonal(vertice v1, vertice v2, vertice* vn)
+{
     vertice vetor;
     vetor.x = v1.x - v2.x;
     vetor.y = v1.y - v2.y;
@@ -106,28 +109,32 @@ void CalculoOrtogonal(vertice v1, vertice v2, vertice* vn){
     vn->y /= v_length;
 }
 
-void drawTriangle(vertice v1, vertice v2, vertice v3, vertice v4){
+void drawTriangle(vertice v1, vertice v2, vertice v3, vertice v4)
+{
     vertice vetorNormal;
     vertice v[4] = {{v1.x, v1.y,  v1.z},
-                    {v2.x, v2.y,  v2.z},
-                    {v3.x,  v3.y,  v3.z},
-                    {v4.x,  v4.y, v4.z}};
+        {v2.x, v2.y,  v2.z},
+        {v3.x,  v3.y,  v3.z},
+        {v4.x,  v4.y, v4.z}
+    };
 
     triangle t[2] = {{v[0], v[1], v[2]},
-                     {v[2], v[3], v[0]}};
+        {v[2], v[3], v[0]}
+    };
 
     glBegin(GL_TRIANGLES);
-        for(int i = 0; i < 2; i++) // triangulos
-        {
-            CalculaNormal(t[i], &vetorNormal); // Passa face triangular e endereço do vetor normal de saída
-            glNormal3f(vetorNormal.x, vetorNormal.y,vetorNormal.z);
-            for(int j = 0; j < 3; j++) // vertices do triangulo
-                glVertex3d(t[i].v[j].x, t[i].v[j].y, t[i].v[j].z);
-        }
+    for(int i = 0; i < 2; i++) // triangulos
+    {
+        CalculaNormal(t[i], &vetorNormal); // Passa face triangular e endereço do vetor normal de saída
+        glNormal3f(vetorNormal.x, vetorNormal.y,vetorNormal.z);
+        for(int j = 0; j < 3; j++) // vertices do triangulo
+            glVertex3d(t[i].v[j].x, t[i].v[j].y, t[i].v[j].z);
+    }
     glEnd();
 }
 
-void drawSolido(vertice v1, vertice v2){
+void drawSolido(vertice v1, vertice v2)
+{
     vertice ortogonal;
     CalculoOrtogonal(v1, v2, &ortogonal);
 
@@ -165,17 +172,18 @@ void drawSolido(vertice v1, vertice v2){
     v10.y = v6.y;
     v10.z = v2.z;
 
-    if(grupos[indexGrupoAtual].size() > 2){
-        v3 = oldVector[1];
-        v4 = oldVector[0];
-        v5 = oldVector[2];
-        v6 = oldVector[3];
+    if(grupos[indexGrupoAtual].size() > 2 && entraIf)
+    {
+        v3 = oldVector[0];
+        v7 = oldVector[1];
+        v8 = oldVector[2];
+        v4 = oldVector[3];
     }
 
-    oldVector[0] = v8;
-    oldVector[1] = v7;
-    oldVector[2] = v9;
-    oldVector[3] = v10;
+    oldVector[0] = v5;
+    oldVector[1] = v9;
+    oldVector[2] = v10;
+    oldVector[3] = v6;
 
 
     //1 face do solido
@@ -196,9 +204,11 @@ void drawSolido(vertice v1, vertice v2){
     //6 face do solido
     drawTriangle(v8,v7,v9,v10);
 
+    entraIf = true;
 }
 
-void showInfoOnTitle(int group,float height){
+void showInfoOnTitle(int group,float height)
+{
     static char title[256] = {0};
     char aux1[32];
     sprintf(aux1,"Grupo atual: %.1d, ",group);
@@ -213,23 +223,27 @@ void showInfoOnTitle(int group,float height){
     glutSetWindowTitle(title);
 }
 
-void desenhaEixos2D(){
+void desenhaEixos2D()
+{
     glDisable(GL_LIGHTING);
     glBegin(GL_LINES);
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex3f(-10.0, 0.0, 0.0);
-        glVertex3f( 10.0, 0.0, 0.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(-10.0, 0.0, 0.0);
+    glVertex3f( 10.0, 0.0, 0.0);
 
-        glColor3f(0.0, 1.0, 0.0);
-        glVertex3f(0.0, -10.0, 0.0);
-        glVertex3f(0.0,  10.0, 0.0);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0, -10.0, 0.0);
+    glVertex3f(0.0,  10.0, 0.0);
     glEnd();
     glEnable(GL_LIGHTING);
 }
 
-void modela3D(){
-    for(int i = 0; i < grupos.size(); i++){
-        for(list<vertice>::iterator it = grupos[i].begin(); it != grupos[i].end(); it++){
+void modela3D()
+{
+    for(int i = 0; i < grupos.size(); i++)
+    {
+        for(list<vertice>::iterator it = grupos[i].begin(); it != grupos[i].end(); it++)
+        {
             vertice v1;
             v1.x = it->x;
             v1.y = it->y;
@@ -237,7 +251,8 @@ void modela3D(){
             v1.espessura = it->espessura;
 
             it++;
-            if(it != grupos[i].end()){
+            if(it != grupos[i].end())
+            {
                 vertice v2;
                 v2.x = it->x;
                 v2.y = it->y;
@@ -245,19 +260,95 @@ void modela3D(){
                 v2.espessura = it->espessura;
 
                 drawSolido(v1,v2);
-            }else{
+            }
+            else
+            {
                 break;
             }
             it--;
         }
+        entraIf = false;
     }
 }
 
-void carregaModelo(){}
+void carregaModelo()
+{
+    //Espaço como separador de string
+    string line;
+    string delimiter = " ";
 
-void salvaModelo(){}
+    char nomeArquivo [100];
+    printf("Digite nome do modelo a ser carregado: \n");
+    cin >> nomeArquivo;
+    strcat(nomeArquivo,".txt");
 
-void calculaCoordenadasPonto(float x, float y){
+    ifstream myfile (nomeArquivo);
+
+    //Descarta grupos anteriores
+    grupos.erase(grupos.begin(), grupos.end());
+    grupos.resize(10);
+
+    indexGrupoAtual = 0;
+
+    if (myfile.is_open())
+    {
+        size_t pos = 0;
+        std::string token;
+
+        while ( getline (myfile,line) )
+        {
+            int j = 0;
+            float auxArray[4];
+
+            //Separa campos para serem armazenados
+            while ((pos = line.find(delimiter)) != std::string::npos) {
+                token = line.substr(0, pos);
+                line.erase(0, pos + delimiter.length());
+                auxArray[j] = stof(token);
+                j++;
+
+            }
+                vertice *novo = new vertice();
+                novo->x = auxArray[0];
+                novo->y = auxArray[1];
+                novo->z = auxArray[2];
+                novo->espessura = auxArray[3];
+                grupos[indexGrupoAtual].push_back(*novo);
+        }
+        myfile.close();
+        indexGrupoAtual++;
+    }
+    else cout << "Unable to open file";
+}
+
+void salvaModelo()
+{
+    FILE *arq;
+    int result;
+
+    char nomeArquivo[100];
+    for(int i = 0; i < grupos.size(); i++)
+    {
+        printf("Digite o nome do arquivo para salvar o grupo %d: \n",i);
+        cin >> nomeArquivo;
+
+        strcat(nomeArquivo,".txt");
+        arq = fopen(nomeArquivo, "wt");  /// Cria um arquivo texto para gravação
+
+        for(list<vertice>::iterator it = grupos[i].begin(); it != grupos[i].end(); it++)
+        {
+            /// formato: x y z espessura
+            result = fprintf(arq,"%f %f %f %f \n",it->x, it->y, it->z, it->espessura);
+        }
+    }
+
+    fclose(arq);
+
+    printf("MODELO SALVO \n");
+}
+
+void calculaCoordenadasPonto(float x, float y)
+{
     float pointX = (-10 + x /(width/2) *20);
     float pointY = (10 - y/height * 20);
 
@@ -270,16 +361,18 @@ void calculaCoordenadasPonto(float x, float y){
     grupos[indexGrupoAtual].push_back(*novo);
 }
 
-void desenhaPonto(float x, float y){
+void desenhaPonto(float x, float y)
+{
     glPointSize(10.0);
     glEnable(GL_POINT_SMOOTH);
     glBegin(GL_POINTS);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3f(x, y, 0.0);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(x, y, 0.0);
     glEnd();
 }
 
-void display(void){
+void display(void)
+{
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     /// Index grupo, alturaZPonto
@@ -304,9 +397,12 @@ void display(void){
 
 
     /*desenha ponto 2D*/
-    if(click == 1){
-        for(int i = 0; i < grupos.size(); i++){
-            for(list<vertice>::iterator it = grupos[i].begin(); it != grupos[i].end(); it++){
+    if(click == 1)
+    {
+        for(int i = 0; i < grupos.size(); i++)
+        {
+            for(list<vertice>::iterator it = grupos[i].begin(); it != grupos[i].end(); it++)
+            {
                 desenhaPonto(it->x, it->y);
             }
         }
@@ -330,51 +426,56 @@ void display(void){
     /*Objetos 3D*/
     glMatrixMode (GL_MODELVIEW);
     glPushMatrix();
-        glRotatef( rotationY, 0.0, 1.0, 0.0 );
-        glRotatef( rotationX, 1.0, 0.0, 0.0 );
-        //Desenha objeto 3D
-        modela3D();
+    glRotatef( rotationY, 0.0, 1.0, 0.0 );
+    glRotatef( rotationX, 1.0, 0.0, 0.0 );
+    //Desenha objeto 3D
+    modela3D();
     glPopMatrix();
 
     glutSwapBuffers();
 }
 
-void idle (){
+void idle ()
+{
     glutPostRedisplay();
 }
 
-void reshape (int w, int h){
+void reshape (int w, int h)
+{
     width = w;
     height = h;
 }
 
-void keyboard (unsigned char key, int x, int y){
+void keyboard (unsigned char key, int x, int y)
+{
 
     switch (tolower(key))
     {
-        case 27:
-            exit(0);
-            break;
-        case '.':
+    case 27:
+        exit(0);
+        break;
+    case '.':
         //Aumenta espessura do modelo 3D
-            espessuraGlobal +=0.2;
-            break;
-        case ',':
-            if(espessuraGlobal > 0.4){
-                espessuraGlobal -= 0.2;
-            }
-            break;
-        case 's':
-            salvaModelo();
-            break;
-        case 'l':
-            carregaModelo();
-            break;
+        espessuraGlobal +=0.2;
+        break;
+    case ',':
+        if(espessuraGlobal > 0.4)
+        {
+            espessuraGlobal -= 0.2;
+        }
+        break;
+    case 's':
+        salvaModelo();
+        break;
+    case 'l':
+        carregaModelo();
+        break;
     }
 }
 
 // Motion callback
-void motion(int x, int y ){
+void motion(int x, int y )
+{
     rotationX += (float) (y - last_y);
     rotationY += (float) (x - last_x);
 
@@ -384,7 +485,8 @@ void motion(int x, int y ){
 }
 
 // Mouse callback
-void mouse(int button, int state, int x, int y){
+void mouse(int button, int state, int x, int y)
+{
     if ( button == GLUT_LEFT_BUTTON && state == GLUT_DOWN )
     {
         last_x = x;
@@ -395,7 +497,8 @@ void mouse(int button, int state, int x, int y){
     }
     if ( button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN )
     {
-        if(grupos[indexGrupoAtual].size() > 0){
+        if(grupos[indexGrupoAtual].size() > 0)
+        {
             grupos[indexGrupoAtual].pop_back();
         }
     }
@@ -413,40 +516,64 @@ void specialKeysPress(int key, int x, int y)
 {
     switch(key)
     {
-        case GLUT_KEY_UP:
-            //Aumenta altura do ponto criado
-            alturaZPonto++;
-            break;
-        case GLUT_KEY_DOWN:
-            //Diminui altura do ponto criado
-            if(alturaZPonto > 1)
-                 alturaZPonto--;
-            break;
-        case GLUT_KEY_RIGHT:
-            //Troca grupo e cria se necessário
-            indexGrupoAtual++;
-            if(indexGrupoAtual >= grupos.size()){
-                list<vertice> grupo1;
-                grupos.push_back(grupo1);
-            }
-            break;
-        case GLUT_KEY_LEFT:
-            //troca grupo
-            if(indexGrupoAtual > 0){
-                indexGrupoAtual--;
-            }
-            break;
-        default:
-            break;
-        case GLUT_KEY_F12:
-            //Modo Fullscreen
-            fullSreen = true;
+    case GLUT_KEY_UP:
+        //Aumenta altura do ponto criado
+        alturaZPonto++;
+        break;
+    case GLUT_KEY_DOWN:
+        //Diminui altura do ponto criado
+        if(alturaZPonto > 1)
+            alturaZPonto--;
+        break;
+    case GLUT_KEY_RIGHT:
+        //Troca grupo e cria se necessário
+        indexGrupoAtual++;
+        if(indexGrupoAtual >= grupos.size())
+        {
+            list<vertice> grupo1;
+            grupos.push_back(grupo1);
+        }
+        break;
+    case GLUT_KEY_LEFT:
+        //troca grupo
+        if(indexGrupoAtual > 0)
+        {
+            indexGrupoAtual--;
+        }
+        break;
+    default:
+        break;
+    case GLUT_KEY_F12:
+        //Modo Fullscreen
+        fullSreen = !fullSreen;
+        if(fullSreen)
+        {
+            glutFullScreen();
+        }
+        else
+        {
+            glutReshapeWindow(800,600);
+            glutPositionWindow(100,100);
+        }
     }
     glutPostRedisplay();
 }
 
-void exibeMenu(){
-    printf("");
+void exibeMenu()
+{
+    printf("--------------Menu-----------------\n");
+    printf("Opcoes de interação:\n");
+    printf("LEFT(DIRECIONAL) Retorna ao grupo anterior. \n");
+    printf("RIGHT(DIRECIONAL) Avança para o proximo grupo. \n");
+    printf("UP(DIRECIONAL) Aumenta altura do ponto a ser criado. \n");
+    printf("DOWN(DIRECIONAL) Diminui altura do ponto a ser criado. \n");
+    printf("F12 Modo Fullscreen. \n");
+    printf("s Salvar o modelo. \n");
+    printf("l Carregar modelo.\n");
+    printf(". Aumenta espessura do modelo gerado.\n");
+    printf(", Diminui espessura do modelo gerado.\n");
+    printf("ESC para sair.\n");
+    printf("-------------------------------------\n");
 }
 /// Main
 int main(int argc, char** argv)
